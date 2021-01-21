f5d36747b04fd05cc9d445e8b7eaf18b58531f09
oscar <oscarros@kth.se>
2021-01-21 22:08:04 +0100

    add actiontype enums to parser tests

--- src/test/java/lagersaldo/InputParserTest.java
+++ src/test/java/lagersaldo/InputParserTest.java
@@ -13,30 +13,30 @@ public class InputParserTest {
 
     @ParameterizedTest(name = "Should return SellInventoryAction when parsing valid Sell command \"{0}\"")
     @ValueSource(strings = { "S1", "S100000"})
-    public void testParseSellCommand(String input) {
+    public void testParseSellActionType(String input) {
         IInventoryAction action = InputParser.parseInput(input);
-        assertEquals(SellInventoryAction.class, action.getClass());
+        assertEquals(ActionType.Sell, action.getActionType());
     }
 
     @ParameterizedTest(name = "Should return DeliverInventoryAction when parsing valid Deliver command \"{0}\"")
     @ValueSource(strings = { "I1", "I100000" })
-    public void testParseDeliverCommand(String input) {
+    public void testParseDeliverActionType(String input) {
         IInventoryAction action = InputParser.parseInput(input);
-        assertEquals(DeliverInventoryAction.class, action.getClass());
+        assertEquals(ActionType.Deliver, action.getActionType());
     }
 
     @Test
-    public void testParseRetriveInventoryCommand() {
+    public void testParseRetriveInventoryActionType() {
         IInventoryAction action = InputParser.parseInput("L");
-        assertEquals(RetrieveInventoryAction.class, action.getClass());
+        assertEquals(ActionType.Retrieve, action.getActionType());
     }
 
     @ParameterizedTest(name = "Should throw exception when parsing invalid input \"{0}\"")
     @EmptySource
     @ValueSource(strings = { "S-1", "I-1", "I", "S", "L200", "Q" })
     public void testParseBadInput(String input) {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            IInventoryAction action = InputParser.parseInput(input);
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputParser.parseInput(input);
         });
     }
 
