3e066db23ab8bf4654e445e7b6c0bc7ad89bb821
oscar <oscarros@kth.se>
2021-01-21 22:07:53 +0100

    reformat code

--- src/main/java/lagersaldo/InputParser.java
+++ src/main/java/lagersaldo/InputParser.java
@@ -5,37 +5,51 @@ import java.util.regex.Pattern;
 
 public class InputParser {
 
-    private static boolean validInput(String input) {
-        Pattern validPattern = Pattern.compile("^([SI]{1}\\d{1,6})$|^L$");
+    private static boolean isValidInput(String input) {
+        String validActionTypePattern = "^([SI]{1}\\d{1,6})$|^L$";
+        Pattern validPattern = Pattern.compile(validActionTypePattern);
         Matcher matcher = validPattern.matcher(input);
-        if (matcher.find()) {
-            return true;
+        boolean foundValidPattern = matcher.find();
+        return foundValidPattern;
+    }
+
+    private static ActionType parseActionTypeString(String input) throws IllegalArgumentException {
+        StringBuilder sb = new StringBuilder(input);
+        String actionString = sb.substring(0, 1);
+        switch (actionString) {
+            case "S":
+                return ActionType.Sell;
+            case "L":
+                return ActionType.Retrieve;
+            case "I":
+                return ActionType.Deliver;
+            default:
+                throw new IllegalArgumentException("Invalid input: " + input);
         }
-        return false;
+    }
+
+    private static int parseQuantityString(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        String quantityString = sb.substring(1);
+        int quantity = Integer.parseInt(quantityString);
+        return quantity;
     }
 
     public static IInventoryAction parseInput(String input) throws IllegalArgumentException {
-        if (!validInput(input)) {
+        if (!isValidInput(input)) {
             throw new IllegalArgumentException("Invalid input: " + input);
         }
-        if (input.length() == 1) {
-            switch (input) {
-                case "L":
-                    return new RetrieveInventoryAction();
-                default:
-                    throw new IllegalArgumentException("Invalid input: " + input);
-            }
-        }
-
-        StringBuilder sb = new StringBuilder(input);
-        String actionString = sb.substring(0, 1);
-        String quantityString = sb.substring(1);
-        int quantity = Integer.parseInt(quantityString);
-        switch (actionString) {
-            case "S":
+        ActionType actionType = parseActionTypeString(input);
+        int quantity;
+        switch (actionType) { 
+            case Sell:
+                quantity = parseQuantityString(input);
                 return new SellInventoryAction(quantity);
-            case "I":
+            case Deliver:
+                quantity = parseQuantityString(input);
                 return new DeliverInventoryAction(quantity);
+            case Retrieve:
+                return new RetrieveInventoryAction();
             default:
                 throw new IllegalArgumentException("Invalid input: " + input);
         }
